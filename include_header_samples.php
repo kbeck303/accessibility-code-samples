188029da93e1a14d76b64f3739e39ff3f6de402c
kbeck303 <kbeck303@gmail.com>
2017-11-15 08:32:09 -0500

    remove unneded file

--- include_header_samples.php
+++
@@ -1,4 +0,0 @@
-<header class="cc-branding">
-  <a href="/index.php"><img class="cc-branding__logo" src="/img/oomph_wordmark_red.svg" alt="" width="90"></a>
-  <h1 class="cc-branding__subtitle">Accessibility Code Samples and Solutions</h1>
-</header>
