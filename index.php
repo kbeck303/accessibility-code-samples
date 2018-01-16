d44efd61e0256a5f2180bc7028532373eed86584
kbeck303 <kbeck303@gmail.com>
2018-01-16 12:19:55 -0500

    make some edits and add some pages

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <main class="presentation__main presentation__main--max-width">
       <h1>Welcome</h1>
       <p>During this presentation we will be reviewing code samples that fail accessibility testing, discuss why they failed, and identify how to remediate the code to comply with WCAG 2.0 standards. We will also cover common tools and resources that will help while you audit, remediate, and test your websites.</p>
+      <p>This presentation is up on GitHub <a href="https://github.com/kbeck303/accessibility-code-samples" title="View my presentation on GitHub. This link will open in a new window." target="_blank">https://github.com/kbeck303/accessibility-code-samples</a></p>
+      <p>The code samples are on CodePen <a href="https://codepen.io/kbeck303/" title="View my presentation code on CodePen. This link will open in a new window." target="_blank">https://codepen.io/kbeck303/</a></p>
     </main>
     <?php include 'include_footer.php';?>
   </body>
---
+++ pages/questions.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html class="js no-touchevents">
+  <head>
+    <title>Accessibility Code Samples and Solutions</title>
+    <?php include '../include_meta.php';?>
+  </head>
+  <body class="presentation">
+    <?php include '../include_header.php';?>
+    <main class="presentation__main presentation__main--max-width">
+      <h1>Questions</h1>
+      <ol>
+        <li></li>
+      </ol>
+    </main>
+    <?php include '../include_footer.php';?>
+  </body>
+</html>
