f66e47cbd47d0102760596bc90c37c442984a46e
kbeck303 <kbeck303@gmail.com>
2018-01-17 12:16:08 -0500

    updates

--- samples/form_labels/page_display.php
+++ samples/form_labels/page_display.php
@@ -11,7 +11,6 @@
       <!-- CodePen Code -->
       <p data-height="400" data-theme-id="0" data-slug-hash="yPXpeN" data-default-tab="html,result" data-user="kbeck303" data-embed-version="2" data-pen-title="Field Labels (Search Form): Accessibility Code Samples and Solutions" class="codepen">See the Pen <a href="https://codepen.io/kbeck303/pen/yPXpeN/">Field Labels (Search Form): Accessibility Code Samples and Solutions</a> by kbeck303 (<a href="https://codepen.io/kbeck303">@kbeck303</a>) on <a href="https://codepen.io">CodePen</a>.</p>
       <script async src="https://production-assets.codepen.io/assets/embed/ei.js"></script>
-
       <h2>Notes, Tips, and Reference Materials</h2>
       <ol>
         <li>Try to be as descriptive as you can when adding text for screen readers.</li>
